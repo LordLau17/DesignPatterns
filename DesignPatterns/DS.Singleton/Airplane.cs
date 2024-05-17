5c49888e683aa2f0836a4cc405841dfc2e55cae5
Laurens van Dijk <laurensrobertvandijk@gmail.com>
2024-05-18 01:03:36 +0200

    Initial setup for design patterns + finished singleton

---
+++ DesignPatterns/DS.Singleton/Airplane.cs
@@ -0,0 +1,24 @@
+﻿namespace DS.Singleton;
+
+internal class Airplane
+{
+    private static Airplane? _instance;
+
+    private readonly string _name;
+
+    private Airplane()
+    {
+        _name = $"Airplane_#{new Random().Next()}";
+    }
+
+    public static Airplane GetInstance()
+    {
+        _instance ??= new Airplane();
+        return _instance;
+    }
+
+    public string GetAirplaneName()
+    {
+        return _name;
+    }
+}
---
+++ DesignPatterns/DS.Singleton/Program.cs
@@ -0,0 +1,7 @@
+﻿using DS.Singleton;
+
+var airplane = Airplane.GetInstance();
+Console.WriteLine(airplane.GetAirplaneName());
+
+var airplaneSecond = Airplane.GetInstance();
+Console.WriteLine(airplaneSecond.GetAirplaneName());
